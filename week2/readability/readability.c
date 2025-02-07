2c144504aa598d033df5e4f6e5641217a32d820f
Nkululeko Jonas <3524042+nkululekojonas@users.noreply.github.com>
2025-02-07 13:22:32 +1000

    Complete Readability porblem set

---
+++ week2/readability/readability.c
@@ -0,0 +1,65 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+// Function prototype
+int average(string text);
+
+int main(int argc, string argv[])
+{
+    // Prompt the user for input
+    string text = get_string("Text: ");
+
+    // Caulcate grade
+    int index = average(text);
+
+    // Check grade bounds
+    if (index < 1 ) 
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else 
+    {
+        printf("Grade %i\n", index);
+    }
+
+    return 0;
+}
+
+// Calulcate grade
+int average(string text)
+{
+    // Keep count of end of sentences
+    int words = 1;
+    int letters = 0;
+    int sentences = 0;
+
+    // Count the number of letter in text
+    for (int i = 0, len = strlen(text); i < len; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        } 
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+    }
+
+    // Calculate grade
+    float l = (letters / (float) words ) * 100.0;
+    float s = (sentences / (float) words ) * 100.0;
+
+    return round(0.0588 * l - 0.296 * s - 15.8);
+}
