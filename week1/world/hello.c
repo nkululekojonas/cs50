39b87d18586b2e4483b655c6f476a84722919ba1
Nkululeko Jonas <3524042+nkululekojonas@users.noreply.github.com>
2025-03-28 12:16:40 +1000

    Cleaned up week1

--- week1/world/hello.c
+++ week1/world/hello.c
@@ -1,7 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    string name = get_string("What's you'r name? ");
+    printf("hello, %s\n", name);
 }
 
